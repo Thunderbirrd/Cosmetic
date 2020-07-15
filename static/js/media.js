6ffadb5697ef7af734994ab670192ea01f0df952
Krasav4ik <zkonst12@gmail.com>
2020-07-15 20:10:39 +0300

    media not soft appearance

---
+++ static/js/media.js
@@ -0,0 +1,5 @@
+var items = document.querySelector(".line");
+var media = document.querySelector(".content");
+    items.addEventListener("click", function () {
+        media.classList.add("show");
+    });
