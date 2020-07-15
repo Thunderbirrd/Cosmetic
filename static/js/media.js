82a31315d73ad9d1be0a3520208839c029eef1c0
Evgeniy <Evgeniy>
2020-07-15 20:45:10 +0300

    fix

--- static/js/media.js
+++ static/js/media.js
@@ -1,10 +1,10 @@
 var items = document.querySelector(".line");
 var media = document.querySelector(".content");
 
-    items.addEventListener("mouseenter", function () {
-        media.classList.add("show");
-    });
+items.addEventListener("mouseenter", function () {
+    media.classList.add("show");
+});
 
-    items.addEventListener("click", function () {
-        media.classList.remove("show");
-    });
+items.addEventListener("click", function () {
+    media.classList.toggle("show");
+});
