12449cba821e025ad2231055130987eba2e41109
Krasav4ik <zkonst12@gmail.com>
2020-07-16 17:07:32 +0300

    Service (uncompleted)

---
+++ static/js/service.js
@@ -0,0 +1,14 @@
+const selected = document.querySelector(".selected");
+const options_container = document.querySelector(".options_container");
+const optionslist = document.querySelector(".option");
+
+selected.addEventListener("click", () => {
+    options_container.classList.toggle("active");
+});
+
+optionslist.forEach(o => {
+    o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        options_container.classList.remove("active");
+    });
+});
