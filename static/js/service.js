2ff9306a5d8896d5e4ae2ad1a6d084e78e91224d
Krasav4ik <zkonst12@gmail.com>
2020-07-17 01:21:50 +0300

    Time picker almost done

--- static/js/service.js
+++ static/js/service.js
@@ -1,6 +1,7 @@
 const selected = document.querySelector(".selected");
 const options_container = document.querySelector(".options_container");
 const optionslist = document.querySelectorAll(".option");
+const buttom = document.querySelector(".btn");
 
 selected.addEventListener("click", () => {
     options_container.classList.toggle("active");
@@ -12,3 +13,12 @@ optionslist.forEach(o => {
         options_container.classList.remove("active");
     });
 });
+
+// buttom.forEach(item => {
+//     item.addEventListener("click", () => {
+//     buttom.classList.add("active");
+// });
+// });
+
+buttom.addEventListener("click", () => {
+    buttom.classList.add("active")});
