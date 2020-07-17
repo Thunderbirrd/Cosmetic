2725e9b26fc3a7d8c43324bfd4bc87f6975b9b36
Evgeniy <Evgeniy>
2020-07-17 13:53:16 +0300

    service

--- static/js/service.js
+++ static/js/service.js
@@ -1,7 +1,7 @@
 const selected = document.querySelector(".selected");
 const options_container = document.querySelector(".options_container");
 const optionslist = document.querySelectorAll(".option");
-const buttom = document.querySelector(".btn");
+const buttom = document.querySelectorAll(".btn");
 
 selected.addEventListener("click", () => {
     options_container.classList.toggle("active");
@@ -14,11 +14,11 @@ optionslist.forEach(o => {
     });
 });
 
-// buttom.forEach(item => {
-//     item.addEventListener("click", () => {
-//     buttom.classList.add("active");
-// });
-// });
+buttom.forEach(item => {
+    item.addEventListener("click", () => {
+        item.classList.add("active");
+    });
+});
 
-buttom.addEventListener("click", () => {
-    buttom.classList.add("active")});
+// buttom.addEventListener("click", () => {
+//     buttom.classList.add("active")});
