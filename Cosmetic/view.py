864f843e6a597cf83f558ec933929f362ea2fba7
Artem <artemis.gorb.14@gmail.com>
2020-07-18 21:30:36 +0900

    ptoduct_list

--- Cosmetic/view.py
+++ Cosmetic/view.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from django.core.cache import cache
+from .apps.mainapp.models import Product
+from .settings import LOW_CACHE
 
 
 def home(request):
-    return render(request, 'index.html')
-
-
-def shop(request):
-    return render(request, 'index.html')
+    if LOW_CACHE:
+        key = 'products'
+        product_list = cache.get(key)
+        if product_list is None:
+            product_list = Product.objects.filter(is_active=True)
+            cache.set(key, product_list)
+        return render(request, product_list, 'index.html')
 
 
 def contacts(request):
