6c6c17a1c4335ba31b1443796f05f85266b5b48d
Evgeniy <Evgeniy>
2020-07-18 16:20:43 +0300

    added style and logic for shopping window

--- static/js/shopping_window.js
+++ static/js/shopping_window.js
@@ -1,3 +1,66 @@
+const _createImgProductShoppingWindow = (src) => {
+    const div = document.createElement("div")
+    div.classList.add("img_product")
+
+    const img = document.createElement("img")
+    img.width = 80
+    img.height = 80
+    img.src = src
+    img.alt = "Фото продукта"
+
+    div.appendChild(img)
+
+    return div
+}
+
+const _createTitleShoppingWindow = (title) => {
+    const div = document.createElement("div")
+    div.classList.add("title")
+
+    const p = document.createElement("p")
+    p.textContent = title
+
+    div.appendChild(p)
+
+    return div
+}
+
+const _createPriceShoppingWindow = (price) => {
+    const div = document.createElement("div")
+    div.classList.add("price")
+
+    const p = document.createElement("p")
+    p.innerHTML = `Цена: ${price} &#8381;`
+
+    div.appendChild(p)
+
+    return div
+}
+
+const _createCountShoppingWindow = (count) => {
+    const div = document.createElement("div")
+    div.classList.add("count")
+
+    const p = document.createElement("p")
+    p.innerHTML = `Количество: ${count};`
+
+    div.appendChild(p)
+
+    return div
+}
+
+const _createAmountShoppingWindow = (price, count) => {
+    const div = document.createElement("div")
+    div.classList.add("amount")
+
+    const p = document.createElement("p")
+    p.innerHTML = `Сумма: ${price * count} &#8381;`
+
+    div.appendChild(p)
+
+    return div
+}
+
 //загрузка списка покупок
 const loadingShoppingList = () => {
     const list = document.querySelector(".shopping_window .list")
@@ -8,20 +71,42 @@ const loadingShoppingList = () => {
         const li = document.createElement("li")
         li.dataset.title = data.title
 
-        li.appendChild(_createImgProduct(data.src))
-        li.appendChild(_createTitle(data.title))
-        li.appendChild(_createPrice(data.price))
-        li.appendChild(_createCount(data.title, data.count))
-        li.appendChild(_createDelete(data.title))
+        li.appendChild(_createImgProductShoppingWindow(data.src))
+        li.appendChild(_createTitleShoppingWindow(data.title))
+        li.appendChild(_createPriceShoppingWindow(data.price))
+        li.appendChild(_createCountShoppingWindow(data.count))
+        li.appendChild(_createAmountShoppingWindow(data.price, data.count))
 
         list.appendChild(li)
     })
 }
-//loadingShoppingList()
+
+loadingShoppingList()
 
 //в поле номер телефона вводить можно только числа
 document.querySelector(".shopping_window .data_fields .number").addEventListener("keypress", (event) => {
     const data = String.fromCharCode(event.charCode)
 
     if (data < "0" || data > "9") event.preventDefault();
-})
+});
+
+(() => {
+    const additionallyFields = document.querySelectorAll(".shopping_window .data_fields .address .additionally")
+    const initDisplayFields = []
+
+    //сохраняем начальный display у additionallyFields (не должна изменятся их последовательность) 
+    additionallyFields.forEach((field, key) => {
+        initDisplayFields[key] = field.style.display
+    })
+
+    //при клике что город Якутск убираем поля название города, улицы и номер дома
+    document.getElementById("isCityYakutsk").onchange = (ev) => {
+        additionallyFields.forEach((field, key) => {
+            if (ev.target.checked) {
+                field.style.display = "none"
+            } else {
+                field.style.display = initDisplayFields[key]
+            }
+        })
+    }
+})()
