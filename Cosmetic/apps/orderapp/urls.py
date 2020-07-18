1616fbdf73a8e0cf012174ab044117328bba5dd1
Vadim <vadimmm120@yandex.ru>
2020-07-18 21:01:55 +0300

    Ordering

--- Cosmetic/apps/orderapp/urls.py
+++ Cosmetic/apps/orderapp/urls.py
@@ -5,5 +5,6 @@ app_name = "orderapp"
 
 urlpatterns = [
     path('', views.form_basket, name='from_basket'),
-    path('form_order/', views.form_order, name='form_order')
+    path('form_order/', views.form_order, name='form_order'),
+    path('delete_order/', views.delete_order, name='delete_order')
 ]
--- Cosmetic/apps/orderapp/views.py
+++ Cosmetic/apps/orderapp/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from Cosmetic.apps.mainapp.models import Product
 from Cosmetic.apps.orderapp.models import Order
 from Cosmetic.apps.orderapp.models import OrderItem
+from django.core.exceptions import ObjectDoesNotExist
 
 
 @csrf_exempt  # Почитать что это!
@@ -14,76 +15,102 @@ def form_basket(request):
     lst = []
     basket_list = json.load(request)  # Список в корзине
     queryset = Product.objects.filter(is_active=True)  # Set из таблицы
-
     # создание пустого заказа
     new_order = Order()
     new_order.save()
     order_id = new_order.id
+    # заполнение заказанных продуктов
+    for product_id in basket_list:
+        item = OrderItem()
+        item.product_id = product_id
+        item.order_id = order_id
+        item.quantity = basket_list[product_id]
+        item.save()
 
-    for product_basket_id in basket_list:
-        product = queryset.values().get(
-            id=product_basket_id)  # нахождение товара в базе из списка корзины
-        # (скорее всего в финальной версии больше условий когда будут нормальные данные)
-        dif = product.get('quantity') - basket_list[product_basket_id]
+    try:
+        for product_basket_id in basket_list:
+            product = queryset.values().get(
+                id=product_basket_id)  # нахождение товара в базе из списка корзины
+            dif = product['quantity'] - basket_list[product_basket_id]
 
-        if dif >= 0:  # Расчитывает доступный товар
-            product['quantity'] = dif
-        else:
-            is_correct = False
-            basket_list[product_basket_id] = product.get('quantity')
+            if dif >= 0:  # Расчитывает доступный товар
+                product['quantity'] = dif
+            else:
+                is_correct = False
+                basket_list[product_basket_id] = product.get('quantity')
 
-        lst.append(Product(product['id'], product['name'], product['price'],
-                           product['category_id'], product['brand_id'],
-                           product['image'], product['quantity']))
+            lst.append(Product(product['id'], product['name'], product['price'],
+                               product['category_id'], product['brand_id'],
+                               product['image'], product['quantity']))
+
+    except ObjectDoesNotExist:
+        return HttpResponse(json.dumps('Error. Product not found.'))
 
     if is_correct:
         for x in lst:
             x.save()
-        thread = threading.Thread(target=return_products, args=(basket_list, False, order_id, ), daemon=True)
+        thread = threading.Thread(target=return_products, args=(basket_list, order_id,), daemon=True)
         thread.start()
-
+    else:
+        Order.objects.filter(id=order_id).delete()
+        order_id = 0
     return HttpResponse(json.dumps({'id': order_id, 'list': basket_list}))
 
 
-def return_products(product_list, immediately, order_id):
-    if not immediately:
-        time.sleep(15)
+def return_products(product_list, order_id):
+    time.sleep(30)
     orders = Order.objects.filter(id=order_id)
     order = orders.get(id=order_id)
-    # Проверка на наличие заказа
+    # проверка на наличие заказа
     if order.client_phone == "":
         orders.delete()
-        queryset = Product.objects.all()
-        for product_basket_id in product_list:
-            product = queryset.values().get(id=product_basket_id)
-            product['quantity'] = product.get('quantity') + product_list[product_basket_id]
 
-            x = Product(product['id'], product['name'], product['price'],
-                        product['category_id'], product['brand_id'],
-                        product['image'], product['quantity'])
-            x.save()
+        try:
+            for product_basket_id in product_list:
+                product = Product.objects.values().get(id=product_basket_id)
+                product['quantity'] = product.get('quantity') + product_list[product_basket_id]
+                x = Product(product['id'], product['name'], product['price'],
+                            product['category_id'], product['brand_id'],
+                            product['image'], product['quantity'])
+                x.save()
+        except ObjectDoesNotExist:
+            return None
 
 
 @csrf_exempt
 def form_order(request):
-
     data = json.load(request)
 
-    order_information = data['list1']
-    order = Order.objects.get(id=data['id'])
-    order.client_phone = order_information['phone']
-    order.client_address = order_information['address']
-    order.client_name = order_information['name']
-    order.client_surname = order_information['surname']
-    order.order_type = order_information['order_type']
-    order.save()
+    order_information = data['list']
+    try:
+        order = Order.objects.get(id=data['id'])
+        order.client_phone = order_information['phone']
+        order.client_address = order_information['address']
+        order.client_name = order_information['name']
+        order.client_surname = order_information['surname']
+        order.order_type = order_information['order_type']
+        order.save()
+    except ObjectDoesNotExist:
+        return HttpResponse(json.dumps('Error. Product not found.'))
+    return HttpResponse(json.dumps('Success'))
 
-    order_list = data['list2']
-    for product_id in order_list:
-        item = OrderItem()
-        item.product_id = product_id
-        item.order_id = data['id']
-        item.quantity = order_list[product_id]
-        item.save()
 
+@csrf_exempt
+def delete_order(request):  # удаление заказа по id при уходе со страницы офрмления
+    data = json.load(request)
+    try:
+        orders = Order.objects.filter(id=data['id'])
+        items = OrderItem.objects.filter(order_id=data['id'])
+
+        for item in items:  # возвращаение товара в бд (возможно if чтобы просто удалять выполненный заказ)
+            product = Product.objects.values().get(id=item.product_id)
+            product['quantity'] = product.get('quantity') + item.quantity
+            x = Product(product['id'], product['name'], product['price'],
+                        product['category_id'], product['brand_id'],
+                        product['image'], product['quantity'])
+            x.save()
+        orders.delete()
+
+    except ObjectDoesNotExist:
+        return HttpResponse(json.dumps('Error. Product not found.'))
     return HttpResponse(json.dumps('Success'))
