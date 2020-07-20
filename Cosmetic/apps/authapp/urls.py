28fb00533e8fa2707f16a335cabdbdfcbd8b8565
Artem <artemis.gorb.14@gmail.com>
2020-07-20 22:54:33 +0900

    small auth url fix

--- Cosmetic/apps/authapp/urls.py
+++ Cosmetic/apps/authapp/urls.py
@@ -4,8 +4,8 @@ from django.urls import re_path
 app_name = 'authapp'
 
 urlpatterns = [
-    re_path(r'^login$', authapp.login, name='login'),
-    re_path(r'^logout$', authapp.logout, name='logout'),
-    re_path(r'^register$', authapp.register, name='register'),
-    re_path(r'^edit$', authapp.edit, name='edit')
+    re_path(r'^login/$', authapp.login, name='login'),
+    re_path(r'^logout/$', authapp.logout, name='logout'),
+    re_path(r'^register/$', authapp.register, name='register'),
+    re_path(r'^edit/$', authapp.edit, name='edit')
 ]
