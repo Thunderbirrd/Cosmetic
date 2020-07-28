64bab3e5f8a35ee38c1bb2cbdcc70b667a5b2b2d
Evgeniy <Evgeniy>
2020-07-28 19:02:10 +0300

    added transition to calendar from admin and changed title

--- Cosmetic/apps/adminapp/admin.py
+++ Cosmetic/apps/adminapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.utils.html import format_html
+
+# Tweak admin site settings like title, header, 'View Site' URL, etc
+admin.site.site_title = 'Панель администрирования'
+admin.site.site_header = 'Панель администрирования'
+
