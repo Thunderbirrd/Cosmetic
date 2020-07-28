110079985a274ed4e4352f9ff526fadef302e2c0
Artem <artemis.gorb.14@gmail.com>
2020-07-28 18:13:58 +0900

    current visit view + url

--- Cosmetic/apps/adminapp/urls.py
+++ Cosmetic/apps/adminapp/urls.py
@@ -4,6 +4,7 @@ from django.urls import re_path
 app_name = "adminapp"
 
 urlpatterns = [
-    re_path(r'^visits', admin_app.visits_calendar, name='visits calendar')
+    re_path(r'^visits', admin_app.visits_calendar, name='visits calendar'),
+    re_path(r'^visit/(?P<pk>\d+)/$', admin_app.current_visit, name='current visit')
 ]
 
--- Cosmetic/apps/adminapp/views.py
+++ Cosmetic/apps/adminapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 from Cosmetic.apps.mainapp.models import Visit
+from django.shortcuts import render, get_object_or_404
 
 
 @user_passes_test(lambda user: user.is_superuser)
@@ -11,3 +12,15 @@ def visits_calendar(request):
         'visits': Visit.objects.all()
     }
     # render(request, какой-то TEMPLATE, content)
+
+
+@user_passes_test(lambda user: user.is_superuser)
+def current_visit(request, pk):
+    title = f"Запись №{pk}"
+    visit = get_object_or_404(Visit, pk=pk)
+    all_client_visits = Visit.objects.filter(client=visit.client).all()
+    content = {
+        'title': title,
+        'all client visits': all_client_visits
+    }
+    #return render(request, TEMLATE, content)
