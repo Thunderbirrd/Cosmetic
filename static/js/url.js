e0e0309d704022714e59ed65382922576e7942d7
Evgeniy <Evgeniy>
2020-07-29 10:56:53 +0300

    added trycatch in request

--- static/js/url.js
+++ static/js/url.js
@@ -1,5 +1,5 @@
 const Urls = {
-    goToUrl(url){
+    goToUrl(url) {
         window.history.replaceState({
             type: FORWARD,
             newPath: url
@@ -12,15 +12,21 @@ const Urls = {
         window.history.back()
     },
     async checkout(data) {
-        let responce = await fetch("/form_basket/", {
-            method: "Post",
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify(data)
-        })
+        try {
+            let responce = await fetch("/form_basket/", {
+                method: "Post",
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify(data)
+            })
+    
+            if (!responce.ok) throw Error(`Ошибка ${responce.status}`)
 
-        store.stateCheckout = await responce.json()
+            store.stateCheckout = await responce.json()
+        } catch (error) {
+            console.error(error)
+        }
     },
     async buyProducts(id, address, phone, name, surname, order_type) {
         let responce = await fetch("/form_basket/form_order/", {
@@ -30,7 +36,7 @@ const Urls = {
             },
             body: JSON.stringify({
                 id,
-                list: {address, phone, name, surname, order_type}
+                list: { address, phone, name, surname, order_type }
             })
         })
 
@@ -39,28 +45,36 @@ const Urls = {
     async signUpForServices(service_name, date) {
         const data = JSON.stringify({
             service_name,
-            date 
+            date
         })
 
         console.log(data);
 
-        const response = await fetch("/form_service/", {
-            method: "Post",
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-                'X-CSRFToken': CRF_TOKEN + ""
-            },
-            body: data
-        })
+        try {
+            const response = await fetch("/form_service/", {
+                method: "Post",
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8',
+                    'X-CSRFToken': CRF_TOKEN + ""
+                },
+                body: data
+            })
 
-        let result = await response.text()
-        console.log(result)
+            if (!response.ok) throw Error(`Ошибка ${response.status}`)
 
-        if (result === 'Success') {
-            showMessage(result)
-            return;
+            let result = await response.text()
+
+            console.log(result)
+
+            if (result === 'Success') {
+                showMessage(result)
+                return;
+            }
+
+            document.documentElement.innerHTML = result
+            window.history.pushState({}, '', "/auth/login/")
+        } catch (e) {
+            console.error(e)
         }
-
-        document.documentElement.innerHTML = result
     }
 }
