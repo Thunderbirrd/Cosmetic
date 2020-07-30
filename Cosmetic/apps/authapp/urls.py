b8f62bd81881758e70110c1804b9a4b8f5263c93
Artem <artemis.gorb.14@gmail.com>
2020-07-30 16:11:51 +0900

    something works now)

--- Cosmetic/apps/authapp/urls.py
+++ Cosmetic/apps/authapp/urls.py
@@ -6,11 +6,13 @@ from django.urls import re_path
 app_name = 'authapp'
 
 urlpatterns = [
-    re_path(r'^password_reset/$', PasswordResetView.as_view(),
+    re_path(r'^password_reset/$', PasswordResetView.as_view(template_name="forget_password/email.html"),
             name="password_reset"),
-    re_path(r'^password_reset_done/$', PasswordResetDoneView.as_view(), name="password_reset_done "),
-    re_path(r'^password_reset_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-
+    re_path(r'^reset-password/done/$', PasswordResetDoneView.as_view(
+        template_name="forget_password/change_password.html"), name="password_reset_done "),
+    re_path(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+            PasswordResetConfirmView.as_view(template_name="forget_password/enter_code.html"),
+            name='password_reset_confirm'),
     re_path(r'^login/$', authapp.login, name='login'),
     re_path(r'^logout/$', authapp.logout, name='logout'),
     re_path(r'^register/$', authapp.register, name='register'),
