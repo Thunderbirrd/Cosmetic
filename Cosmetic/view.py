85bec4c5b8fb4805ce03ec433ffaea000dfea569
Vadim <vadimmm120@yandex.ru>
2020-08-01 19:05:20 +0300

    function visits changed

--- Cosmetic/view.py
+++ Cosmetic/view.py
@@ -1,8 +1,8 @@
+import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.cache import cache
 from django.views.decorators.csrf import csrf_exempt
-
 from .apps.mainapp.models import Product, Visit, Service
 from .settings import LOW_CACHE
 import json
@@ -16,33 +16,29 @@ def home(request):
         if product_list is None:
             product_list = Product.objects.filter(is_active=True)
             cache.set(key, product_list)
-
+            
         content = {
             'title': title,
             'products': product_list,
-            'occupied_dates': visits(),
+            'occupied_dates': visits(datetime.date.today()),
             'service_names': services(),
         }
         return render(request, 'index.html', content)
 
 
-def visits():
-    visit_list = Visit.objects.all()
+def visits(date):
+    visit_list = Visit.objects.filter(date=date)
     occupied_dates = []
 
     for visit in visit_list:
-        occupied_dates.append({'date': visit.date, 'time': visit.time})
+        occupied_dates.append(visit.time)
     return occupied_dates
 
 
 @csrf_exempt
 def refresh(request):
-    visit_list = Visit.objects.all()
-    occupied_dates = []
-
-    for visit in visit_list:
-        occupied_dates.append({'date': visit.date, 'time': visit.time})
-    return HttpResponse(json.dumps(occupied_dates))
+    date = json.loads(request)
+    return HttpResponse(json.dumps(visits(date)))
 
 
 def services():
@@ -51,6 +47,7 @@ def services():
 
 def calendar(request):
     return render(request, 'calendar/index.html')
+
 
 def history_client(request):
     return render(request, 'history_client/history_client.html')
