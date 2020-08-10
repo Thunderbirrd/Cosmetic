d8c213fd629c893ae69ad46ef6842cc150f27eef
Vadim <vadimmm120@yandex.ru>
2020-08-10 17:35:53 +0300

    articles images

--- Cosmetic/apps/mainapp/views.py
+++ Cosmetic/apps/mainapp/views.py
@@ -1,7 +1,7 @@
 import json
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from Cosmetic.apps.mainapp.models import Article
+from Cosmetic.apps.mainapp.models import Article, ImageForArticle
 
 
 @csrf_exempt
@@ -9,7 +9,13 @@ def all_articles(request):
     articles_list = []
     articles = Article.objects.all()
     for article in articles:
-        articles_list.append({'id': article.id, 'title': article.title})
+        images_list = []
+        images = ImageForArticle.objects.filter(article_id=article.id)
+
+        for image in images:
+            images_list.append(image.main_image)
+
+        articles_list.append({'id': article.id, 'title': article.title, 'images': images})
     return HttpResponse(json.dumps(articles_list, ensure_ascii=False))
 
 
