5c1de2ad45840bf41440a5c2b7b91bf5c3520b5f
Artem <artemis.gorb.14@gmail.com>
2020-08-20 20:07:59 +0900

    i need to pull

--- Cosmetic/view.py
+++ Cosmetic/view.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.cache import cache
 from django.views.decorators.csrf import csrf_exempt
-from django.core import serializers
 from .apps.mainapp.models import Product, Visit, Service, ProductCompilation, Months
 from .settings import LOW_CACHE
 import json
